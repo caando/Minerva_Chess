8d53f42c190ee2cfa95d191358fb756ff6786aa2
caando <zhangjikun907475@gmail.com>
2023-05-27 17:53:47 +0800

    fix: kill stockfish process after move

--- bot/services/engine.ts
+++ bot/services/engine.ts
@@ -24,6 +24,8 @@ class EngineService {
     engine.stdin.write(`go movetime ${COMPUTING_TIME_PER_MOVE}\n`);
     await this.sleep(COMPUTING_TIME_PER_MOVE * 2);
     const match = lastMessage.toString().match(/bestmove ([+a-zA-Z0-9]+)/);
+    engine.stdin.write(`quit\n`);
+    engine.kill(7);
     return match ? match[1] : null;
   }
 }
