5a474b2861125b1d6e298ddd2967d1c375c898c0
caando <zhangjikun907475@gmail.com>
2023-06-26 06:52:40 +0800

    feat(uci): communication functions

---
+++ src/uci.h
@@ -0,0 +1,16 @@
+//
+// Created by Jikun on 26/6/23.
+//
+
+#ifndef MINERVA_CHESS_SRC_UNI_H_
+#define MINERVA_CHESS_SRC_UNI_H_
+
+#include "variables.h"
+
+int inputWaiting();
+
+void readInput();
+
+static void communicate();
+
+#endif //MINERVA_CHESS_SRC_UNI_H_
