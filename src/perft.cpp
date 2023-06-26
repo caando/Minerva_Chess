3f3531643897e3b59ee6bf19a73dd69d4887ba0f
caando <zhangjikun907475@gmail.com>
2023-06-26 08:59:12 +0800

    feat(perft): perft

---
+++ src/perft.cpp
@@ -0,0 +1,93 @@
+//
+// Created by Jikun on 26/6/23.
+//
+
+#include "variables.h"
+#include "movegen.h"
+#include "util.h"
+#include "types.h"
+
+// perft driver
+static inline void perftDriver(int depth) {
+  // reccursion escape condition
+  if (depth == 0) {
+    // increment nodes count (count reached positions)
+    nodes++;
+    return;
+  }
+
+  // create move list instance
+  moves move_list[1];
+
+  // generate moves
+  generateMoves(move_list);
+
+  // loop over generated moves
+  for (int move_count = 0; move_count < move_list->count; move_count++) {
+    // preserve board state
+    copyBoard();
+
+    // make move
+    if (!makeMove(move_list->moves[move_count], all_moves))
+      // skip to the next move
+      continue;
+
+    // call perft driver recursively
+    perft_driver(depth - 1);
+
+    // take back
+    takeBack();
+  }
+}
+
+// perft test
+void perftTest(int depth) {
+  // reset nodes count
+  nodes = 0ULL;
+
+  printf("\n     Performance test\n\n");
+
+  // create move list instance
+  moves move_list[1];
+
+  // generate moves
+  generateMoves(move_list);
+
+  // init start time
+  long start = getTimeMs();
+
+  // loop over generated moves
+  for (int move_count = 0; move_count < move_list->count; move_count++) {
+    // preserve board state
+    copyBoard();
+
+    // make move
+    if (!makeMove(move_list->moves[move_count], all_moves))
+      // skip to the next move
+      continue;
+
+    // cummulative nodes
+    long cummulative_nodes = nodes;
+
+    // call perft driver recursively
+    perftDriver(depth - 1);
+
+    // old nodes
+    long old_nodes = nodes - cummulative_nodes;
+
+    // take back
+    takeBack();
+
+    // print move
+    printf("     move: %s%s%c  nodes: %ld\n", squareToCoordinates[getMoveSource(move_list->moves[move_count])],
+           squareToCoordinates[getMoveTarget(move_list->moves[move_count])],
+           getMovePromoted(move_list->moves[move_count]) ? promotedPieces[getMovePromoted(move_list->moves[move_count])]
+                                                         : ' ',
+           old_nodes);
+  }
+
+  // print results
+  printf("\n    Depth: %d\n", depth);
+  printf("    Nodes: %lld\n", nodes);
+  printf("     Time: %ld\n\n", getTimeMs() - start);
+}
---
+++ src/perft.h
@@ -0,0 +1,12 @@
+//
+// Created by Jikun on 26/6/23.
+//
+
+#ifndef MINERVA_CHESS_SRC_PERFT_H_
+#define MINERVA_CHESS_SRC_PERFT_H_
+
+static inline void perftDriver(int depth);
+
+void perftTest(int depth);
+
+#endif //MINERVA_CHESS_SRC_PERFT_H_
