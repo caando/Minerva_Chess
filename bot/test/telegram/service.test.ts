e3b93a67fda7072009d9c0b311875d67f877c06a
Woo Jia Hao <woojiahao1234@gmail.com>
2023-07-15 23:59:21 +0800

    Complete test cases for startGame

--- bot/test/telegram/service.test.ts
+++ bot/test/telegram/service.test.ts
@@ -1,12 +1,18 @@
+import { Chess } from "chess.js";
 import { exec } from "child_process";
 import {
   addGame,
   addUser,
+  getGame,
   getUser,
   whiteStartPos
 } from "../../services/database";
+import { engine } from "../../services/engine";
 import { ongoingGameError } from "../../services/telegram/errors";
 import { startGame } from "../../services/telegram/service";
+import { Game } from "../../services/models/game";
+
+jest.mock("../../services/engine");
 
 // Before every test, create the database from scratch to ensure data purity
 beforeEach(() => {
@@ -46,7 +52,19 @@ describe("start game", () => {
   });
 
   test("creates game with player as BLACK and makes engine move first", async () => {
-    const johndoe = await getUser("johndoe");
-    console.log(johndoe);
+    // Need to comment because TS complains that there isn't a method
+    // Jest adds this method to let us mock the engine calls without using the actual engine
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    engine.getMove.mockImplementation(async () => "e2e4");
+    // Ensure BLACK
+    jest.spyOn(global.Math, "random").mockReturnValue(0.2);
+    const game = await startGame("johndoe");
+    const chess = new Chess(whiteStartPos);
+    chess.move("e2e4");
+    expect(game).toBeDefined();
+    expect(game).toBeInstanceOf(Game);
+    if (game instanceof Error) return;
+    expect(game.dataValues.fen).toBe(chess.fen());
   });
 });
