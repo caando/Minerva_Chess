01ff44e37f530f2e8f01f660c68450ad15724e10
Woo Jia Hao <woojiahao1234@gmail.com>
2023-07-23 21:28:47 +0800

    Remove logging for engine output

--- bot/services/engine.ts
+++ bot/services/engine.ts
@@ -17,7 +17,6 @@ class EngineService {
 
     let lastMessage: Buffer = Buffer.alloc(0);
     engine.stdout.on("data", (data: Buffer) => {
-      console.log("data:", data.toString());
       lastMessage = data;
     });
     engine.stdin.write(`ucinewgame\n`);
